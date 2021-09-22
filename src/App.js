94ccf24baf2cdb3da38619a8ae98d669a9197324
Cruzm430 <mark@Marks-MBP.home>
2021-09-21 20:10:44 -0400

    inital commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+      <header className="App-header">
+        <p>
+          Pretentious Bastards 
+        </p>
+      </header>
+  );
+}
+
+export default App;
